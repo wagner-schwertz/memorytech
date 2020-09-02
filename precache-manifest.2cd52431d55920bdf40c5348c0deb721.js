95a4acd9f92e66965287f099bb049b4b256c5ade
Wagner Schwertz dos Santos <wagner.santos@ntconsult.com.br>
2020-09-02 11:54:38 -0300

    Updates

---
+++ precache-manifest.2cd52431d55920bdf40c5348c0deb721.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c71d6151b9a506415b63638b8a6d23dc",
+    "url": "/memorytech/index.html"
+  },
+  {
+    "revision": "cddc205c4d4699e61c89",
+    "url": "/memorytech/static/js/2.08a035d2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/memorytech/static/js/2.08a035d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d8d4488d013a0157091",
+    "url": "/memorytech/static/js/main.d6bb1c2b.chunk.js"
+  },
+  {
+    "revision": "e9efc6605d2f229bbd31",
+    "url": "/memorytech/static/js/runtime-main.587fa64b.js"
+  }
+]);
