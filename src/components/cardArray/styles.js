7d797dc44f6ed6a644c365f0b7e7f291e7563c76
Wagner Schwertz dos Santos <wagner.santos@ntconsult.com.br>
2020-09-02 04:29:33 -0300

    clean up

--- src/components/cardArray/styles.js
+++ src/components/cardArray/styles.js
@@ -1,12 +1,5 @@
 import styled from "styled-components";
 
-export const DeckArray2 = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  width: 660px;
-  height: 660px;
-`;
-
 export const DeckArray = styled.div`
   display: grid;
   height: 99%;
